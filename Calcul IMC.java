fa99544258350cc639f8e5c0f8efbb5b6f0399bd
ACVivant <39029651+ACVivant@users.noreply.github.com>
2018-05-07 08:20:22 +0200

    Ajout de la class Imc (Main)

--- Calcul IMC.java
+++ Calcul IMC.java
@@ -28,3 +28,15 @@ class Patient {
 		return imc;
 	}
 }
+
+class Imc {
+    public static void main(String[] args) {
+
+        Patient quidam = new Patient();
+        quidam.init(74.5, 1.75);
+        quidam.afficher();
+        System.out.println("IMC : " + quidam.imc());
+        quidam.init( -2.0, 4.5);
+        quidam.afficher();
+    }
+}
