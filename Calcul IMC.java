eac3dee0fe781ad0515f68fa835a1b71aa958177
ACVivant <39029651+ACVivant@users.noreply.github.com>
2018-05-07 08:19:41 +0200

    Création de la class Patient

--- Calcul IMC.java
+++ Calcul IMC.java
@@ -1,1 +1,30 @@
+class Patient {
+	double poids;
+	double hauteur;
+	double imc = 0.0;
+	
+	// méthode pour entrer les données patient poids et taille
+	public void init(double poidsin, double hauteurin) {
+		poids=0.0;
+		hauteur=0.0;
+		if (poidsin >0 && hauteurin >0) {
+			poids = poidsin;
+			hauteur = hauteurin;}
+	}
 
+	// méthode pour afficher les données du patient
+	public void afficher() {
+		System.out.print("Patient : ");
+		System.out.printf("%.1f", poids);
+		System.out.print(" kg pour ");
+		System.out.printf("%.1f", hauteur);
+		System.out.println(" m");
+	}
+	
+	public double imc() {
+		 imc = 0.0;
+		if (hauteur !=0) {
+			imc = poids/(hauteur*hauteur);}
+		return imc;
+	}
+}
