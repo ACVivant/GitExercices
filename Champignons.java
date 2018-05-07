24f65854c76e3d54bf23845a19d7053e3b7bd4be
ACVivant <39029651+ACVivant@users.noreply.github.com>
2018-05-07 08:18:40 +0200

    Ajout de l'introduction sur le principe du jeu

--- Champignons.java
+++ Champignons.java
@@ -1,1 +1,5 @@
+/*
+ * Le but de ce jeu est de faire découvrir à l'ordinateur quel est le champignon auquel on pense.
+ * L'ordinateur va nous poser 3 questins au maximum. Nous pouvons répondre seulement par oui ou non.
+ */
 
