cecc3ad8f53d31151eadcd7cc85fd2a42bfc763d
Anne-Charlotte <annecharlotte.vivant@gmail.com>
2018-05-07 08:27:45 +0200

    Changement du texte donnant l'IMC

--- Calcul IMC.java
+++ Calcul IMC.java
@@ -35,7 +35,7 @@ class Imc {
         Patient quidam = new Patient();
         quidam.init(74.5, 1.75);
         quidam.afficher();
-        System.out.println("IMC : " + quidam.imc());
+        System.out.println("Votre IMC est de : " + quidam.imc());
         quidam.init( -2.0, 4.5);
         quidam.afficher();
     }
